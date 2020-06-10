a14008d9aff446e14f3ab70042487b2577601743
sophie-apples <sophieannemurray@gmail.com>
2020-06-10 10:29:11 +0000

    added email js into contact.html

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -41,8 +41,7 @@ function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
     if (!username) {
-        $("#gh-user-data").html(
-            `<h2>Please enter a GitHub username</h2>`);
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return;
     }
 
@@ -64,13 +63,17 @@ function fetchGitHubInformation(event) {
         
         function(errorResponse) {
             if (errorResponse.status === 404) {
-                4("#gh-user-data").html(
+                $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
                 console.log(errorResponse);
-                $("gh-user-data").html(
+                $("#gh-user-data").html(
                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
         });
 }
-$ (document).ready(fetchGitHubInformation);
+
+$(document).ready(fetchGitHubInformation);
